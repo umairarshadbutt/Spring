853e9cd9f9c31d3f86773abb8095f2bde7f3e4fa
umairarshad <umair6622@gmail.com>
2019-09-05 16:31:59 +0500

    Creating Entities with JPA

---
+++ EnTiTy/src/main/java/com/example/entity/entity/Product.java
@@ -0,0 +1,59 @@
+package com.example.entity.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String category;
+    private String type;
+    private String sector;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+}
---
+++ EnTiTy/src/test/java/com/example/entity/entity/EntityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.entity.entity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
