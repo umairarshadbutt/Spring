4bc5c82af4a14f87a3158b9ef8c5d00ad1b45033
umairarshad <umair6622@gmail.com>
2019-09-06 12:18:08 +0500

    findByCategoryAndNameIn

--- Different Ways to Query Data with JPA and Spring Data/src/main/java/com/example/repository/repository/RepositoryApplication.java
+++ Different Ways to Query Data with JPA and Spring Data/src/main/java/com/example/repository/repository/RepositoryApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -51,15 +52,26 @@ public class RepositoryApplication implements CommandLineRunner {
 //            LOG.info("Products found: "+ product.toString());
 //        }
 
+
 //        Product resultProduct = productRepository.findByType("Custom");
 //        LOG.info("CUSTOM type of product found "+ resultProduct.toString());
 
 
-        List<Product> results = productRepository.findByDescriptionAndCategory("This is tester product","Test");
-        for (Product product: results){
-            LOG.info("Matching results are: "+ product.toString());
+
+//        List<Product> results = productRepository.findByDescriptionAndCategory("This is tester product","Test");
+//        for (Product product: results){
+//            LOG.info("Matching results are: "+ product.toString());
+//        }
+
+
+        List<String> names = new ArrayList<>();
+        names.add("Tester Product");
+        // names.add("Another Tester Product");
+
+        List<Product> resultProducts = productRepository.findByCategoryAndNameIn("Test",names);
+
+        for(Product product: resultProducts){
+            LOG.info("Matching results for findByCategoryAndNameIn: "+ product.toString());
         }
-
-
     }
 }
--- Different Ways to Query Data with JPA and Spring Data/src/main/java/com/example/repository/repository/repository/ProductRepository.java
+++ Different Ways to Query Data with JPA and Spring Data/src/main/java/com/example/repository/repository/repository/ProductRepository.java
@@ -11,4 +11,5 @@ public interface ProductRepository extends JpaRepository<Product, String> {
 
     Product findByType(String type);
     List<Product> findByDescriptionAndCategory(String description, String category);
+    List<Product> findByCategoryAndNameIn(String category, List<String> name);
 }
