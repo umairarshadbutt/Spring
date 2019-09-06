7d0d7c7165a852533492612b0a1f87bf6daa4431
umairarshad <umair6622@gmail.com>
2019-09-06 12:57:54 +0500

    project added for Deleting Data with JPA and Spring Data

---
+++ Deleting Data with JPA and Spring Data/src/main/java/com/example/repository/repository/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.example.repository.repository.repository;
+
+import com.example.repository.repository.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+
+    Product findByType(String type);
+    List<Product> findByDescriptionAndCategory(String description, String category);
+    List<Product> findByCategoryAndNameIn(String category, List<String> name);
+}
